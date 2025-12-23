f6743ea0779c596adb63e5a057c2f8750f14b48a
Edudlufetips1 <aq8829@outlook.com>
2025-12-22 19:58:59 -0500

    Asteroids game update

---
+++ main.py
@@ -0,0 +1,30 @@
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import pygame
+from logger import log_state
+from player import Player
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+def main():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+    dt = 0
+    while True:
+        log_state()
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()  
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
