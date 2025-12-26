d1062e0d7264145c434bb43cf8da316c6b8bbbdf
Edudlufetips1 <aq8829@outlook.com>
2025-12-25 22:09:46 -0500

    added collision tracking to asteroids game

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
 import pygame
-
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+import sys
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE_SECONDS
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-pygame.init()
-
-clock = pygame.time.Clock()
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)  # type: ignore
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    field = AsteroidField()
     dt = 0
+
     while True:
         log_state()
-    
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        updatable.update(dt)
+        
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()  
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
